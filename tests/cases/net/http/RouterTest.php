b3ab2399ee14b48ecb725ec0890e58a524865d8b
Robert Cuadra <robert@prgmtk.com>
2011-12-13 16:40:53 -0800

    added tests for router

--- tests/cases/net/http/RouterTest.php
+++ tests/cases/net/http/RouterTest.php
@@ -8,8 +8,47 @@
 
 namespace li3_rest\tests\cases\net\http;
 
+use lithium\action\Request;
+use lithium\net\http\Route;
+use li3_rest\net\http\Router;
+use li3_rest\net\http\Resource;
+use lithium\action\Response;
+
 class RouterTest extends \lithium\test\Unit {
-	
+
+	protected $_routes = array();
+
+	public function setUp() {
+		$this->request = new Request();
+		$this->_routes = Router::get();
+		Router::reset();
+	}
+
+	public function tearDown() {
+		Router::reset();
+
+		foreach ($this->_routes as $route) {
+			Router::connect($route);
+		}
+	}
+
+	public function testResource() {
+		Router::resource('Posts');
+		$resource = Resource::config();
+		$types = $resource['types'];
+		$typeKeys = array_keys($types);
+		$routes = Router::get();
+
+		$this->assertEqual(count($types), count($routes));
+
+		for($i = 0; $i < count($types); $i++) {
+			$type = $types[$typeKeys[$i]];
+			$route = $routes[$i]->export();
+			$this->assertEqual(str_replace('{:resource}', 'posts', $type['template']), $route['template']);
+			$this->assertEqual($type['params']['http:method'], $route['meta']['http:method']);
+		}
+	}
+
 }
 
 ?>
